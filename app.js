60f0628623b744d842aead0a1b71293e5e7b444e
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-08-12 13:28:37 -0500

    Add conditional to not allow a blank post

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ function($scope){
   
   // addPost function retrieves the title entered in the form which is bound to the $scope variable 'title'
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
-    $scope.title = '';
-  };
+    // doesn't allow user to submit a blank post
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
 }]);
 
 
