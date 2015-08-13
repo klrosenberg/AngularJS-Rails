ba67b217b3e5cb011bb03cec64be7e610f6427bd
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-08-13 09:16:20 -0500

    Add fake comments to addPost function

--- app.js
+++ app.js
@@ -52,7 +52,11 @@ angular.module('flapperNews', ['ui.router'])
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link, 
-        upvotes: 0});
+        upvotes: 0
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]});
         $scope.title = '';
         $scope.link = '';
       };
